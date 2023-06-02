8f0cd9c4c7280fda2fe5e0635598a30c61d4e205
Sandro Petrovski <sandro@tpeng.cc>
2023-06-02 13:03:00 -0400

    feat: pong framework

--- game/global.gd
+++ game/global.gd
@@ -12,4 +12,4 @@ func _change_scene(path):
 	scene.free()
 	scene = ResourceLoader.load(path).instantiate()
 	get_tree().root.add_child(scene)
-	get_tree().scene = scene
+	get_tree().current_scene = scene
